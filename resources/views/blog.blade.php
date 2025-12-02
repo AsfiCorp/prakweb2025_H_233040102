0a144efe10563336b415d5decbea3f776562ea07
Athaillah Sulthan <athaillahsulthan@MacBook-Pro-M3-Athaillah.local>
2025-12-02 22:33:10 +0700

    Mengerjakan modul Laravel 1 - Instalasi dan MVC

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:title>
+        Blog
+    </x-slot:title>
+    
+    <h3 class="text-xl">Ini adalah Halaman Blog</h3>
+    <p class="mt-4">Berisi kumpulan artikel seputar teknologi dan koding.</p>
+</x-layout>
